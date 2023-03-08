96cef2f418f5c00382a3b0dca705af03cf578067
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-08 14:47:41 +0100

    add working if else

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,22 @@ console.log (userAge)
 // calcola il prezzo del viaggio sapendo che
 // prezzo biglietto 0.21 €/km
 
-const ticketPrice = (travelLenght) * 0.21
-// perché va solo con le parentesi?
-console.log(ticketPrice);
+const ticketPrice = ((travelLenght) * 0.21)
+// --------------------------------perché va solo con le parentesi?
+console.log (ticketPrice);
+
+// SE <18 sconto 20%
+// SE >65 sconto 40%
+
+if (userAge <= 18) {
+   const youngPrice = ticketPrice - (ticketPrice / 100 * 20)
+   alert(`il tuo prezzo giovane é ${youngPrice} `)
+
+}else if (userAge >= 65){
+    const elderPrice = ticketPrice - (ticketPrice /100 * 40)
+    alert (`il tuo prezzo anziani é ${elderPrice}`)
+}
+// --------------------------perché non va il console.log dentro gli if ?
+else {
+    alert (`il tuo prezzo normale é ${ticketPrice}`)
+}
